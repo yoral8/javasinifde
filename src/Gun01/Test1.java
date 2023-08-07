7c729f39507cc5d604deb8a5db5968316b36e78c
yoral8 <ysrorl@hotmail.com>
2023-08-07 22:26:57 +0300

    ekleme yaşar26

--- src/Gun01/Test1.java
+++ src/Gun01/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Team 7");
         System.out.println("gebzeden selamlar ");
+        System.out.println("olmadı");
+
     }
 
 }
