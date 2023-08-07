6d3d956171c60d7c3f471246741de55242470582
yoral8 <ysrorl@hotmail.com>
2023-08-07 22:05:49 +0300

    ekleme yaşar2

--- src/Gun01/Test1.java
+++ src/Gun01/Test1.java
@@ -3,7 +3,7 @@ package Gun01;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Team 7");
-        System.out.println("gebzeden selamlar");
+        System.out.println("gebzeden selamlar ");
     }
 
 }
